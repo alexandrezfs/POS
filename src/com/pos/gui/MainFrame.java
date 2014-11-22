1422470f4a228da5a70b707814280ea9b50a6025
NGUYEN Alexandre <alex.nr@hotmail.fr>
2014-11-22 18:47:58 +0100

    Initial commit

---
+++ src/com/pos/gui/MainFrame.java
@@ -0,0 +1,15 @@
+package com.pos.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame() {
+
+        this.setSize(new Dimension(1300, 800));
+        this.setVisible(true);
+
+    }
+
+}
---
+++ src/com/pos/init/Init.java
@@ -0,0 +1,15 @@
+package com.pos.init;
+
+import com.pos.gui.MainFrame;
+
+import javax.swing.*;
+
+public class Init {
+
+    public static void main(String[] args) {
+
+        JFrame mainFrame = new MainFrame();
+
+    }
+
+}
