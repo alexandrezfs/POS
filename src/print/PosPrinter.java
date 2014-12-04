430ac3d230e83fa7f9b43e25ad6d1d324f11d01b
NGUYEN Alexandre <alex.nr@hotmail.fr>
2014-12-04 22:57:34 +0100

    Printer class created

---
+++ src/print/PosPrinter.java
@@ -0,0 +1,18 @@
+package print;
+
+/**
+ * Created by alexandrenguyen on 04/12/14.
+ */
+public class PosPrinter {
+
+    private String toPrint;
+
+    public String getToPrint() {
+        return toPrint;
+    }
+
+    public void setToPrint(String toPrint) {
+        this.toPrint = toPrint;
+    }
+
+}
