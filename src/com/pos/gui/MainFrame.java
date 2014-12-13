0e56147bc528597353aa06db0487858d5e03ecb7
NGUYEN Alexandre <alex.nr@hotmail.fr>
2014-12-13 19:13:16 +0100

    Added another fields components for init

--- src/com/pos/gui/MainFrame.java
+++ src/com/pos/gui/MainFrame.java
@@ -1,10 +1,15 @@
 package com.pos.gui;
 
+import javafx.scene.control.ComboBox;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class MainFrame extends JFrame {
 
+    /*
+    JPANELS
+     */
     private JPanel globalPanel;
     private JPanel productPanel;
     private JPanel directExchangePanel;
@@ -13,6 +18,9 @@ public class MainFrame extends JFrame {
     private JPanel totalPanel;
     private JPanel validPanel;
 
+    /*
+    PRODUCT INPUT ZONE
+     */
     private JTextField priceField;
     private JTextField ibanField;
     private JTextField productTitleField;
@@ -21,11 +29,40 @@ public class MainFrame extends JFrame {
     private JButton submitButton;
     private JComboBox productTypeCombo;
 
+    /*
+    DIRECT EXCHANGE INPUT ZONE
+     */
+    private JTextField directExchangeAmountField;
+    private JTextField directExchangeClientField;
+
+    /*
+    PURCHASE INPUT ZONE
+     */
+    private JTextField couponAmountField;
+    private JComboBox couponStoreCombo;
+    private JTextField couponIdField;
+
+    /*
+    DISCOUNT INPUT ZONE
+     */
+    private JTextField discountClientField;
+    private JTextField discountAmountField;
+    private JComboBox discountAmountTypeCombo;
+
     public MainFrame() {
 
         this.setTitle("Logiciel de caisse v2.0");
         this.setSize(new Dimension(1300, 800));
         this.setVisible(true);
+        this.initializePanels();
+
+        initializeProductInputZone();
+        initializeDirectExchangeInputZone();
+        initializePurchaseInputZone();
+        initializeDiscountInputZone();
+    }
+
+    private void initializePanels() {
 
         globalPanel = new JPanel();
         productPanel = new JPanel();
@@ -36,8 +73,6 @@ public class MainFrame extends JFrame {
         validPanel = new JPanel();
 
         this.setContentPane(globalPanel);
-
-        initializeProductInputZone();
     }
 
     private void initializeProductInputZone() {
@@ -50,6 +85,26 @@ public class MainFrame extends JFrame {
         submitButton = new JButton();
         productTypeCombo = new JComboBox();
 
+    }
+
+    private void initializeDirectExchangeInputZone() {
+
+        directExchangeAmountField = new JTextField();
+        directExchangeClientField = new JTextField();
+    }
+
+    private void initializePurchaseInputZone() {
+
+        couponAmountField = new JTextField();
+        couponIdField = new JTextField();
+        couponStoreCombo = new JComboBox();
+    }
+
+    private void initializeDiscountInputZone() {
+
+        discountAmountField = new JTextField();
+        discountAmountTypeCombo = new JComboBox();
+        discountClientField = new JTextField();
     }
 
 }
