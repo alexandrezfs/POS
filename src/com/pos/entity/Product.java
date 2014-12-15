7c9fd356f608d50ad786f3ecb887f8fb51b9ec77
NGUYEN Alexandre <alex.nr@hotmail.fr>
2014-12-15 22:17:29 +0100

    Created product entity

---
+++ src/com/pos/entity/Product.java
@@ -0,0 +1,55 @@
+package com.pos.entity;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by alexandrenguyen on 15/12/14.
+ */
+public class Product {
+
+    private String code;
+    private BigDecimal price;
+    private String title;
+    private String author;
+    private String editor;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getEditor() {
+        return editor;
+    }
+
+    public void setEditor(String editor) {
+        this.editor = editor;
+    }
+}
---
+++ src/com/pos/entity/Transaction.java
@@ -0,0 +1,7 @@
+package com.pos.entity;
+
+/**
+ * Created by alexandrenguyen on 15/12/14.
+ */
+public class Transaction {
+}
