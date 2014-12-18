02a4c39074fb95fd8a3920717693361b9a7a4ece
NGUYEN Alexandre <alex.nr@hotmail.fr>
2014-12-18 22:13:56 +0100

    Installed JDBC

--- src/com/pos/entity/Transaction.java
+++ src/com/pos/entity/Transaction.java
@@ -4,4 +4,7 @@ package com.pos.entity;
  * Created by alexandrenguyen on 15/12/14.
  */
 public class Transaction {
+
+    private String date;
+
 }
---
+++ src/com/pos/report/ReportService.java
@@ -0,0 +1,14 @@
+package com.pos.report;
+
+/**
+ * Created by alexandrenguyen on 18/12/14.
+ */
+public class ReportService {
+
+    public void generateProductReport() {
+
+
+
+    }
+
+}
